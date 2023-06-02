df8a964f4e8d4efa483e8b5a049c29d17b35a3e0
AlpenGold0656 <alpengold150@gmail.com>
2023-06-02 19:14:43 +0500

    new project

--- js/main.js
+++ js/main.js
@@ -679,39 +679,41 @@ let mockData = [
 
 let loadingHTML = `
 
-    <div class="d-flex align-items-center justify-content-center">
-    <div class="loader m-4">
-
-
-    </div>
-    </div> 
+<div class="d-flex align-items-center justify-content-center">
+<div class="loader m-4">
+</div>
+</div> 
 
 `;
 
 let errorHTML = `
-    <p class=" fs-2 text-center" >
-    <i class="text-danger fa-solid fa-triangle-exclamation"></i>
-    error in fetch
-    </p>
+<p class=" fs-2 text-center" >
+<i class="text-danger fa-solid fa-triangle-exclamation"></i>
+error in fetch
+</p>
 `;
 
 let noTextHTML = `
-    <p class=" fs-2 text-center" >
-    <i class="text-danger fa-solid fa-triangle-exclamation"></i>
-    There is no such domain
-    </p>
+<p class=" fs-2 text-center" >
+<i class="text-danger fa-solid fa-triangle-exclamation"></i>
+There is no such domain
+</p>
 `;
 
 let noTextEntered = `
-    <p class=" fs-2 text-center" >
-    <i class="text-danger fa-solid fa-triangle-exclamation"></i>
-    No text entered
-    </p>
+<p class=" fs-2 text-center" >
+<i class="text-danger fa-solid fa-triangle-exclamation"></i>
+No text entered
+</p>
 `;
+
+let historyDiv = document.querySelector(".history");
+let searchInfo = document.querySelector(".search-info");
+
 const getDomains = async (domain, zone) => {
   try {
     const res = await axios.get(
-      `http://api.domainsdb.info/v1/domains/search?domain=${domain}&zone=${zone}`
+      `https://api.domainsdb.info/v1/domains/search?domain=${domain}&zone=${zone}`
     );
     return res.data;
   } catch (error) {
@@ -719,16 +721,14 @@ const getDomains = async (domain, zone) => {
     return mockData;
   }
 };
-
 let setDomains = async () => {
   let inputSearch = document.querySelector("#input-search").value;
-  let inputCom = document.querySelector("#input-com");
-  let searchInfo = document.querySelector(".search-info");
+  let inputCom = document.querySelector("#input-com").value;
   let topolmadi = true;
 
   searchInfo.innerHTML = loadingHTML;
   if (inputSearch !== "") {
-    let respons = await getDomains();
+    let respons = await getDomains(inputSearch, inputCom);
 
     console.log(respons);
 
@@ -737,12 +737,18 @@ let setDomains = async () => {
       console.log(item);
       if (item.domain.startsWith(String(inputSearch))) {
         console.log(item.domain);
+        localStorage.setItem("history", item.domain);
+        historyDiv.innerHTML += `
+        
+        <p>${localStorage.getItem("history")}</p>
+
+        `;
 
         searchInfo.innerHTML += `
         
         <div class="d-flex align-items-center justify-content-between  ">
         <p class="m-0 p-0 fs-5">${item.domain}</p>
-
+        
       <a href="${
         item.MX !== null ? item.MX[0].exchange : "#"
       } " class="text-white">
@@ -751,9 +757,19 @@ let setDomains = async () => {
       </div>
       `;
         topolmadi = false;
-        localStorage.setItem("history", searchInfo.innerHTML);
       }
     });
+
+    searchInfo.innerHTML += `
+    
+    <button
+      id="search-button"
+      class="btn-damen"
+      onclick="history()">
+                  History
+    </button>
+             
+    `;
 
     if (topolmadi) {
       searchInfo.innerHTML = noTextHTML;
@@ -763,5 +779,23 @@ let setDomains = async () => {
   }
 };
 
-let historyDiv = document.querySelector(".history");
-historyDiv.innerHTML = localStorage.getItem("history");
+let history = () => {
+  searchInfo.innerHTML = "";
+  searchInfo.innerHTML = historyDiv.innerHTML;
+
+  searchInfo.innerHTML += `
+  <button
+      id="search-button"
+      class="btn-damen"
+      onclick="exit()">
+                  Orqaga
+    </button>
+
+
+
+  `;
+};
+
+let exit = () => {
+  setDomains();
+};
